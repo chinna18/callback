07fde1fbce8fb33410f12ea334bfeb107fb9a567
Puli ChinnaObulaReddy <77271346+chinna18@users.noreply.github.com>
2021-04-03 08:06:03 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var a = 10;
+var i=0;
+
+    setTimeout(()=>{document.getElementById('display').innerHTML=a;i++;
+       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
+       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
+       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
+       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
+       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
+       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
+       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
+       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
+       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
+       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
+       setTimeout(()=>{document.getElementById('display').innerHTML="Happy Independence Day!";},1000);
+    },1000);},1000);},1000);},1000);},1000);},1000);},1000);},1000);},1000);},1000);},1000);
+
+var p = document.createElement("h1");
+p.setAttribute('id','display');
+p.setAttribute('style','text-align:center');
+document.body.append(p);
