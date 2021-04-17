29599db59cdf4d93f1da2db7761c7c27083ff458
Puli ChinnaObulaReddy <77271346+chinna18@users.noreply.github.com>
2021-04-17 13:06:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 var a = 10;
 var i=0;
 
-    setTimeout(()=>{document.getElementById('display').innerHTML=a;i++;
-       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
-       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
-       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
-       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
-       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
-       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
-       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
-       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
-       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
-       setTimeout(()=>{document.getElementById('display').innerHTML=a-i;i++;
-       setTimeout(()=>{document.getElementById('display').innerHTML="Happy Independence Day!";},1000);
-    },1000);},1000);},1000);},1000);},1000);},1000);},1000);},1000);},1000);},1000);},1000);
-
+    setTimeout(() => {
+  document.getElementById("display").innerHTML = a;
+  i++;
+  setTimeout(() => {
+    document.getElementById("display").innerHTML = a - i;
+    i++;
+    setTimeout(() => {
+      document.getElementById("display").innerHTML = a - i;
+      i++;
+      setTimeout(() => {
+        document.getElementById("display").innerHTML = a - i;
+        i++;
+        setTimeout(() => {
+          document.getElementById("display").innerHTML = a - i;
+          i++;
+          setTimeout(() => {
+            document.getElementById("display").innerHTML = a - i;
+            i++;
+            setTimeout(() => {
+              document.getElementById("display").innerHTML = a - i;
+              i++;
+              setTimeout(() => {
+                document.getElementById("display").innerHTML = a - i;
+                i++;
+                setTimeout(() => {
+                  document.getElementById("display").innerHTML = a - i;
+                  i++;
+                  setTimeout(() => {
+                    document.getElementById("display").innerHTML = a - i;
+                    i++;
+                    setTimeout(() => {
+                      document.getElementById("display").innerHTML = a - i;
+                      i++;
+                      setTimeout(() => {
+                        document.getElementById("display").innerHTML =
+                          "Happy Independence Day!";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
 var p = document.createElement("h1");
 p.setAttribute('id','display');
 p.setAttribute('style','text-align:center;color:green');
